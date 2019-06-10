5d03ffa60a57b5739f091f05a35794eabe27f10b
yangyongmao <2523308718@qq.com>
2019-06-10 20:33:59 +0800

    yym0610

---
+++ app/Http/Controllers/Admin/MenuController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 杨永茂
+ * Date: 2019/6/10
+ * Time: 15:04
+ */
+
+namespace App\Http\Controllers\Admin;
+use Illuminate\Http\Request;
+use DB;
+use App\Http\Controllers\Controller;
+
+class MenuController extends Controller{
+
+    public function index()
+    {
+
+    }
+
+}
