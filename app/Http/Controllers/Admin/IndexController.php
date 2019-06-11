a10c320ed8e7420b8180b691782004927f473d9e
yangyongmao <2523308718@qq.com>
2019-06-11 08:44:38 +0800

    0611

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -5,27 +5,14 @@
  * Date: 2019/6/10
  * Time: 11:21
  */
-
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use DB;
 use App\Http\Controllers\Controller;
-<<<<<<< HEAD
-class IndexController extends Controller
-{
-    public function index(){
-
-//        echo __adminStatic__;die();
-
-        return view("admin\index");
-=======
 
 class IndexController extends Check
 {
     public function index(){
         return view("admin\index\\index");
->>>>>>> upstream/master
     }
-
 }
