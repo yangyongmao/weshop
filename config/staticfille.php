73bfc042648b97edb84fd520e88dbf9b95ab1488
greedisgoodsay <1923473051@qq.com>
2019-06-12 09:48:15 +0800

    'new'

--- config/staticfille.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: lixinyuan
- * Date: 2019/6/10
- * Time: 11:35
- */
-define("__adminStatic__","/adminStatic");
--- resources/views/Admin/index/index.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app');
-@section('content')
-    @foreach($data as $v)
-        <label for="">{{$v->a_id}}</label>
-    @endforeach
-@endsection
