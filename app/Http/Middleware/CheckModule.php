19e628169f185ee01ad11da221c5a04a35f75271
sharejia <sharejia@outlook.com>
2019-06-25 10:03:30 +0800

    JXC-06.25

--- app/Http/Middleware/CheckModule.php
+++ app/Http/Middleware/CheckModule.php
@@ -15,14 +15,6 @@ class CheckModule
      */
     public function handle($request, Closure $next)
     {
-//        $controllerName = request()->route()->getActionName();
-//        $strat = strripos($controllerName,'\\');
-//        $end = strripos($controllerName,'@');
-//        $length = $end - $strat;
-//        $controllerName = strtolower(substr($controllerName,$strat + 1,$length - 11));
-//        $actionName = strtolower(request()->route()->getActionMethod());
-//        $module = $controllerName.'/'.$actionName;
-
         $json = json_encode($request->session()->get('adminAccess'));
         $array = json_decode($json,true);
 
